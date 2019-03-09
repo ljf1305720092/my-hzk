f989dbf1723cc93cbd3884e0a2c73f438a2e16f9
liuhuaqiang <1305720092@qq.com>
2019-03-09 20:45:24 +0800

    完成全局路由配置

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 
+function Login () {
+  return (
+    <div>   
+        Home
+    </div>
+  )
+}
+
+function Home () {
+  
+  return <div>Home</div>
+}
+
 class App extends Component {
+
+  handle = () => {
+    
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Login}></Route>
+          <Route path="/home" component={Home}></Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
