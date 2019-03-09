5ce88d2ea9961a43ec7b732f8cb56794a0013db3
liuhuaqiang <1305720092@qq.com>
2019-03-09 23:52:21 +0800

    完成登陆接口调用和token存储

--- src/login.js
+++ src/login.js
@@ -1,7 +1,42 @@
 import React from 'react';
 import { Form, Icon, Button, Divider } from 'semantic-ui-react'
+import axios from 'axios'
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+    handleUsername = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+    handlePassword = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+    submit = async () => {
+        // 获取表单数据
+        // 调用后台接口验证数据  http://47.96.21.88:8086/users/login
+        // 客户端接收到token信息然后储存到sessionStorage中
+        // 页面跳转
+        let url = 'http://47.96.21.88:8086/users/login';
+        let ret = await axios.post(url,{
+            uname: this.state.username,
+            pwd: this.state.password
+        })
+        let data = ret.data
+        if (data.meta.status===200) {
+            sessionStorage.setItem('mytoken',data.data.token)
+            console.log(data)
+        }
+        console.log(data.meta.msg)
+    }
     render() {
         return (
             <div className='login-container'>
@@ -16,6 +51,8 @@ class Login extends React.Component {
                         size='big' 
                         iconPosition='left' 
                         name='username'
+                        value={this.state.username}
+                        onChange={this.handleUsername}
                         placeholder='请输入用户名...' 
                         />
                         <Form.Input 
@@ -23,10 +60,12 @@ class Login extends React.Component {
                         required 
                         size='big' 
                         iconPosition='left' 
-                        name='username'
+                        name='password'
+                        value={this.state.password}
+                        onChange={this.handlePassword}
                         placeholder='请输入密码...' 
                         />
-                        <Button fluid color='green'>登陆</Button>
+                        <Button onClick={this.submit} fluid color='green'>登陆</Button>
                         <Divider horizontal>----</Divider>
                     </Form>
                 </div>
