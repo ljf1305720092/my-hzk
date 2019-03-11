6ee2e8cfa3502817684baf85a544881e2d9d58b1
liuhuaqiang <1305720092@qq.com>
2019-03-11 09:33:58 +0800

    登陆功能token效验

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import './login.css'
 import 'semantic-ui-css/semantic.min.css'
@@ -7,6 +7,32 @@ import Main from './module/my/main'
 import Login from './login'
 
 
+function Accc (props) {
+  // console.log(props)
+  let Istoken = sessionStorage.getItem('mytoken')? true: false
+  let {path, component:Component} = props
+  return (
+    <Route path={path} render={()=>{
+      let com = Istoken?<Component/> : <Redirect to='/'/>
+      return com
+    }}></Route>
+  )
+}
+
+function Abbb (props) {
+let {path,component:Component} = props
+let Istoken = sessionStorage.getItem('mytoken') ? true:false
+  return (
+    <Route path={path} render= {() => {
+    let sz = Istoken? <Component/>:<Redirect to='/'></Redirect>
+    return sz
+    }}/>
+  )
+}
+
+function Aacc () {
+  return <div>新来的中的新来的</div>
+}
 class App extends Component {
 
   handle = () => {
@@ -17,7 +43,9 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Login}></Route>
-          <Route path="/home" component={Main}></Route>
+          {/* <Route path="/home" component={Main}></Route> */}
+          <Abbb path='/home' component={Main}></Abbb>
+          <Accc path='/abc' component={Aacc}></Accc>
         </Switch>
       </BrowserRouter>
     );
